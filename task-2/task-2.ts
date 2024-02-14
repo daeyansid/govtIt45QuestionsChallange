db32811a9ab438cf83614d0b09b9262f5058c291
Daeyan_server <daeyansidi826@gmail.com>
2024-02-15 02:30:08 +0500

    2nd commit

---
+++ task-2/task-2.ts
@@ -0,0 +1,9 @@
+// 2. Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric,
+// would you like to learn some Python today?”
+
+
+// code:
+
+let myName:string = "Muhammad Daeyan Siddiqui";
+
+console.log(`Hello ${myName}, would you like to learn some Python today?`);
